cb34a651756be565d8ade3f15ec2c0cbf3717ddb
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-05 12:13:12 -0600

    Basic functionality save turn in Supabase done

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,28 @@
 
 import { useState } from "react";
 import TurnCreationForm from "./(modules)/(turns-generation)/(components)/TurnCreationForm/TurnCreationForm";
+import supabase from "./(utils)/supabase";
 
 export default function Home() {
   const [turnsList, setTurnsList] = useState<any[]>([]);
 
-  const onCreateTurn = (formValues: any) => {
+  const onCreateTurn = async (formValues: any) => {
     alert("Turn created");
 
     setTurnsList([...turnsList, formValues]);
+
+    await supabase
+      .from("Turns")
+      .insert({ ...formValues, id: crypto.randomUUID() });
   };
 
   return (
     <div className="">
       <TurnCreationForm onSubmit={onCreateTurn} />
+      {turnsList.length}
+      {turnsList.map((turn, index) => (
+        <div key={index}>{turn.name}</div>
+      ))}
     </div>
   );
 }
