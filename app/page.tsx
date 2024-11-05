7b71d5554cdd0614734ee8bda2fa3a44dcf35f50
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-05 10:39:01 -0600

    Basic way to add turns to turnsList added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import TurnCreationForm from "./(modules)/(turns-generation)/(components)/TurnCreationForm/TurnCreationForm";
 
 export default function Home() {
+  const [turnsList, setTurnsList] = useState<any[]>([]);
+
   const onCreateTurn = () => {
     alert("Turn created");
+
+    setTurnsList([...turnsList, { name: "Carlos" }]);
   };
 
   return (
