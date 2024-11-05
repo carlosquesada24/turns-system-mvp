aca2943597c948609cb7217c4d8f7fabcc11ac59
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-05 10:34:28 -0600

    onCreateTurn function created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import TurnCreationForm from "./(modules)/(turns-generation)/(components)/TurnCreationForm/TurnCreationForm";
 
 export default function Home() {
+  const onCreateTurn = () => {
+    alert("Turn created");
+  };
+
   return (
     <div className="">
-      <TurnCreationForm />
+      <TurnCreationForm onSubmit={onCreateTurn} />
     </div>
   );
 }
