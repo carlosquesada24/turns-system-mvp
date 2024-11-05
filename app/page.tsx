a8291d17bcacd20b0f9005b621dd97c995dcacf2
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-05 12:25:21 -0600

    Save and getAll Turns repositories used in dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import turnsSupabaseRepository from "./(modules)/(turns-generation)/(repositories)/turnsSupabaseRepository";
+
 import TurnCreationForm from "./(modules)/(turns-generation)/(components)/TurnCreationForm/TurnCreationForm";
-import supabase from "./(utils)/supabase";
 
 export default function Home() {
   const [turnsList, setTurnsList] = useState<any[]>([]);
+
+  useEffect(() => {
+    const handleFetchAllTurns = async () => {
+      const fetchedTurnsList = await turnsSupabaseRepository.getAllTurns();
+      setTurnsList(fetchedTurnsList);
+    };
+
+    handleFetchAllTurns();
+  }, []);
 
   const onCreateTurn = async (formValues: any) => {
     alert("Turn created");
 
     setTurnsList([...turnsList, formValues]);
 
-    await supabase
-      .from("Turns")
-      .insert({ ...formValues, id: crypto.randomUUID() });
+    const turnToSave = {
+      ...formValues,
+      id: crypto.randomUUID(),
+    };
+
+    await turnsSupabaseRepository.saveTurn(turnToSave);
   };
 
   return (
