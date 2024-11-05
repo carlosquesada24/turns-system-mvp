3b06a4901ac4a71bff5925fd96918b8693fad6a0
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-05 10:48:00 -0600

    Basic useForm logic added to TurnCreationForm

--- app/(modules)/(turns-generation)/(components)/TurnCreationForm/TurnCreationForm.tsx
+++ app/(modules)/(turns-generation)/(components)/TurnCreationForm/TurnCreationForm.tsx
@@ -4,21 +4,29 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { useForm } from "@/app/(hooks)/useForm";
 
 interface TurnCreationFormProps {
   onSubmit: Function;
 }
 
 const TurnCreationForm = ({ onSubmit }: TurnCreationFormProps) => {
-  const handleSubmit = () => {
-    typeof onSubmit === "function" && onSubmit();
+  const { values: formValues, handleInputChange } = useForm({ name: "" }, {});
+
+  const handleSubmit = (formValues: any) => {
+    typeof onSubmit === "function" && onSubmit(formValues);
   };
 
   return (
     <div>
       <Label htmlFor="name">Nombre</Label>
-      <Input id="name" type="text" placeholder="Carlos" />
-      <Button onClick={handleSubmit}>Crear turno</Button>
+      <Input
+        id="name"
+        type="text"
+        onChange={(e) => handleInputChange("name", e.currentTarget.value)}
+        placeholder="Carlos"
+      />
+      <Button onClick={() => handleSubmit(formValues)}>Crear turno</Button>
     </div>
   );
 };
