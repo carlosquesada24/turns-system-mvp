649ea459c6128ec3ea31066f06a36f351e3a31ae
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-05 10:20:39 -0600

    Unused imports deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import TurnCreationForm from "./(modules)/(turns-generation)/(components)/TurnCreationForm/TurnCreationForm";
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-3xl font-bold">App de turnos</h1>
+      <TurnCreationForm />
     </div>
   );
 }
