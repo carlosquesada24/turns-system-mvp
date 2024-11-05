b206b7064fea27d505b4c81c6129180360f65f36
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-05 12:23:49 -0600

    Simple getAllTurns repository created

--- app/(modules)/(turns-generation)/(repositories)/turnsSupabaseRepository.ts
+++ app/(modules)/(turns-generation)/(repositories)/turnsSupabaseRepository.ts
@@ -9,10 +9,22 @@ interface TurnSupabase extends Turn {
     createdAt: string
 }
 
+const getAllTurns = async (): Promise<Turn[]> => {
+    const { data, error } = await supabase.from('Turns')
+        .select()
+
+    if (error) {
+        console.log({ error })
+    }
+
+    return data as Turn[]
+}
+
 const saveTurn = async (turn: Turn) => {
     return await supabase.from("Turns").insert(turn);
 }
 
 export default {
+    getAllTurns,
     saveTurn,
 }
