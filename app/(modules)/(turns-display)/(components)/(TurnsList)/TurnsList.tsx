6dd91744db39b3b96bc8bc3cfafaa5bd9f5db51e
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-06 10:03:56 -0600

    Basic title added when turnsArray is Empty

--- app/(modules)/(turns-display)/(components)/(TurnsList)/TurnsList.tsx
+++ app/(modules)/(turns-display)/(components)/(TurnsList)/TurnsList.tsx
@@ -6,23 +6,30 @@ interface TurnsListProps {
 }
 
 const TurnsList = ({ turnsList, userTurnId }: TurnsListProps) => {
+  const isValidArray = turnsList && Array.isArray(turnsList);
+  const isEmptyArray = isValidArray && turnsList.length === 0;
+
   return (
     <div>
-      <h2 className="text-xl font-bold">Turnos pendientes</h2>
-      {turnsList &&
-        Array.isArray(turnsList) &&
-        turnsList?.map((turn, index) => {
-          const isUsersTurn = userTurnId === turn.id;
+      <h2 className="text-xl font-bold text-center">Turnos pendientes</h2>
+      <div className="text-center">
+        {isEmptyArray ? (
+          <h2>¡No hay turnos pendientes!</h2>
+        ) : (
+          turnsList?.map((turn, index) => {
+            const isUsersTurn = userTurnId === turn.id;
 
-          return (
-            <div
-              key={index}
-              className={`${isUsersTurn ? "bg-sky-600 text-white" : ""}`}
-            >
-              {turn.name} - #{turn.position}
-            </div>
-          );
-        })}
+            return (
+              <div
+                key={index}
+                className={`${isUsersTurn ? "bg-sky-600 text-white" : ""}`}
+              >
+                {turn.name} - #{turn.position}
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 };
